918363d0f502a0fa729a6ec941f0d9b0e6a31124
Lorenzo More <lorenzo.duarte@edu.pucrs.br>
2024-10-04 22:24:07 +0000

    tried to cross-compile, but its not working

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -4,4 +4,4 @@ cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 cp $BASE_DIR/../custom-scripts/cod.py $BASE_DIR/target/usr/bin
 chmod +x $BASE_DIR/target/usr/bin/cod.py
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
-
+$BASE_DIR/labsisop-buildroot/output/host/usr/bin/i686-buildroot-linux-uclibc-gcc custom-scripts/syscall_test.c -o $BASE_DIR/output/target/usr/bin/syscall_test
