8b52dc43b066e648489d821e8c49c4e4ecfdcc19
Lorenzo More <lorenzo.duarte@edu.pucrs.br>
2024-10-04 23:09:46 +0000

    fixed cross-compilation

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# copy files from host to target
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 cp $BASE_DIR/../custom-scripts/cod.py $BASE_DIR/target/usr/bin
+
+# changing permissions
 chmod +x $BASE_DIR/target/usr/bin/cod.py
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
-$BASE_DIR/labsisop-buildroot/output/host/usr/bin/i686-buildroot-linux-uclibc-gcc custom-scripts/syscall_test.c -o $BASE_DIR/output/target/usr/bin/syscall_test
+
+# compiling c code
+$BASE_DIR/host/usr/bin/i686-buildroot-linux-uclibc-gcc custom-scripts/syscall_test.c -o $BASE_DIR/target/usr/bin/syscall_test
